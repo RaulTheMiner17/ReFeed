a5a6030012d80b9aff55e525c022cd979a111fff
RaulTheMiner17 <raulferns1@gmail..com>
2025-03-28 12:53:46 +0530

    minor ui changes

--- app/src/main/java/com/ran/refeed/ui/screens/CategoriesScreen.kt
+++ app/src/main/java/com/ran/refeed/ui/screens/CategoriesScreen.kt
@@ -10,8 +10,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
